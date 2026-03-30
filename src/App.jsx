7853850fc7daf89b887c2d60a3683d60bdf19a99
salmansahed <salmansahed10@gmail.com>
2026-03-30 06:17:21 +0600

    implement CTA section with responsive layout

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Banner from './components/Banner'
 import Achievements from './components/Achievements'
 import HowItWorks from './components/HowItWorks'
 import Pricing from './components/Pricing'
+import CTA from './components/CTA'
 
 const pricingData = async() => {
   const res = await fetch("PricingData.json");
@@ -22,6 +23,7 @@ function App() {
       <Achievements />
       <HowItWorks />
       <Pricing pricingPromise={pricingPromise} />
+      <CTA/>
     </>
   );
 }
---
+++ src/components/CTA.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const CTA = () => {
+    return (
+      <div className="bg-linear-to-r from-[#4F39F6] to-[#9514FA] py-16 md:py-24 lg:py-30 text-center space-y-3 md:space-y-4 px-4">
+        <h1 className="text-3xl lg:text-[40px] text-white font-black">
+          Ready to Transform Your Workflow?
+        </h1>
+        <p className="text-white">
+          Join thousands of professionals who are already using Digitools to
+          work smarter.Start your free trial today.
+        </p>
+        <div className="flex items-center justify-center gap-4 mt-6 md:mt-10">
+          <button className="btn rounded-full bg-white">
+            <span className="bg-linear-to-r from-[#4F39F6] to-[#9514FA] text-transparent bg-clip-text">
+              Explore Products
+            </span>
+          </button>
+          <button className="btn rounded-full bg-transparent border border-white text-white">
+            View Pricing
+          </button>
+        </div>
+        <p className='text-white'>14-day free trial • No credit card required • Cancel anytime</p>
+      </div>
+    );
+};
+
+export default CTA;
